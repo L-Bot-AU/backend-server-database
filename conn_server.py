bee8b3423e0297404bc96e10a5fc6e3b06dfeb69
TurtleMaster69420 <48506404+TurtleMaster69420@users.noreply.github.com>
2021-02-17 13:09:37 +1100

    Update conn_server.py

--- conn_server.py
+++ conn_server.py
@@ -7,7 +7,7 @@ class ConnServer:
     def __init__(self, SERVER_IP="127.0.0.1", PORT=4485):
         self.SERVER_SOCK = (SERVER_IP, PORT)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 32400000)
+        self.sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 32400000, 5000))
         self.sock.connect(self.SERVER_SOCK)
         self.aesdec = AES.new(b"automate_egggggg", AES.MODE_ECB)
         message = self.sock.recv(1024)
